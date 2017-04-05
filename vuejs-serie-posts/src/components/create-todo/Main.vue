60133d2ac67198f58f22295f8e4dccaf670c14b8
walkerdepaula <walkerdepaula.work@gmail.com>
2017-04-05 07:52:23 -0300

    criando primeiros components

---
+++ vuejs-serie-posts/src/components/create-todo/Main.vue
@@ -0,0 +1,43 @@
+<script>
+  export default {
+    name: 'CreateTodo',
+    data() {
+      return {
+        todoName: '',
+        todoList: []
+      }
+    },
+    methods: {
+      add() {
+        if (this.todoName !== '') {
+          this.todoList.push(this.todoName)
+
+          this.todoName = ''
+
+          this.$refs.todoName.focus()
+        }
+      }
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <h3>Create to do</h3>
+    <form @submit.prevent="add">
+      <div class="form-group">
+        <label class="control-label">Name</label>
+        <input autofocus ref="todoName" type="text" class="form-control" v-model="todoName">
+      </div>
+      <button class="btn btn-default btn-xs" type="submit">Save</button>
+    </form>
+    <hr v-show="todoList.length > 0">
+    <div class="col-md-4" v-for="todo in todoList">
+      <div class="panel panel-default">
+        <div class="panel-body">
+          {{ todo }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ vuejs-serie-posts/src/components/list/Main.vue
@@ -0,0 +1,15 @@
+<script>
+  export default {
+    name: 'listTodo',
+  }
+</script>
+
+<template>
+  <div>
+    <h3>To do list</h3>
+    <form action="#">
+      <label class="control-label">Select a to do list</label>
+      <select class="form-control"></select>
+    </form>
+  </div>
+</template>
